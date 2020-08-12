24952109693bcf8de52d338372b36db7b9d4eb57
Malin Hanak <malinehanak@gmail.com>
2020-08-12 16:38:31 +0200

    Basic style folder

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,93 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body, p, button, article, section, input, h1 {
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 100;
+  }
+
+  a {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  header#header-hook {
+    position: fixed;
+    z-index: 15;
+    top: 0;
+    left: 0;
+
+    max-height: 96px;
+    width: 100%;
+  }
+
+  footer#footer-hook {
+    position: fixed;
+    z-index: 15;
+    bottom: 0;
+    left: 0;
+
+    height: 96px;
+    max-height: 96px;
+    width: 100%;
+
+    background-color: ${(props) => props.theme.colors.mosque};
+    color: ${(props) => props.theme.colors.jetStream};
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  main#root {
+    margin-top: 80px;
+  }
+
+
+  .main-navigation__drawer-nav {
+    height: 100%;
+  }
+
+  .slide-in-left-enter {
+    transform: translateX(-100%);
+  }
+
+  .slide-in-left-enter-active {
+    transform: translateX(0);
+    opacity: 1;
+    transition: all 200ms;
+  }
+
+  .slide-in-left-exit {
+    transform: translateX(0%);
+    opacity: 1;
+  }
+
+  .slide-in-left-exit-active {
+    transform: translateX(-100%);
+    opacity: 0;
+    transition: all 200ms;
+  }
+
+  @media (min-width: 768px) {
+    .main-navigation__menu-btn {
+      display: none;
+    }
+  }
+`;
