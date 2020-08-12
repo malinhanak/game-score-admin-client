5c0e38ed6be08786626a47c6837a43202990aea4
Malin Hanak <malinehanak@gmail.com>
2020-08-12 16:38:46 +0200

    Packages and start up files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import { ThemeProvider } from 'styled-components';
+
 import * as serviceWorker from './serviceWorker';
+import { AuthProvider } from './shared/providers';
+import { GlobalStyle, theme } from './styles';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CookiesProvider>
+    <AuthProvider>
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <GlobalStyle />
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </React.StrictMode>
+    </AuthProvider>
+  </CookiesProvider>,
   document.getElementById('root')
 );
 
