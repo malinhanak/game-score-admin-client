306b62831aa25f70257bcf5a4c444260b79d64e6
Malin Hanak <malinehanak@gmail.com>
2020-08-13 10:35:19 +0200

    Dashboard page and App comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
-  return <div>Admin app</div>;
+  return (
+    <Layout>
+      <Switch>
+        <ProtectedRoute exact path="/" component={Dashboard} />
+        {/* <ProtectedRoute path="/game/:game" component={Game} /> */}
+        <Route path="/auth" component={Auth} />
+        {/* <Route path="*" component={Page404} /> */}
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
