266754437107c39e58508c6832b35012e241be4a
Malin Hanak <malinehanak@gmail.com>
2020-08-13 10:35:05 +0200

    Basic Auth page

---
+++ src/pages/Auth.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Input } from '../components/Input';
+import { Form } from '../styles/Form';
+import { emailPattern } from '../utils/helpers';
+import { authContext } from '../shared/providers/auth-context';
+
+const Auth = () => {
+  const auth = useContext(authContext);
+  const { register, handleSubmit, watch, errors } = useForm();
+  // handleSubmit(
+  //   auth.login(e, history, { name: team, password: password })
+  // )
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log('input values', watch().username);
+  };
+
+  return (
+    <Form
+      className="auth-form"
+      onSubmit={(event) => handleSubmit(onSubmit(event))}
+    >
+      <Input
+        name="username"
+        label="Användarnamn (E-post)"
+        ref={register({
+          required: true,
+          pattern: { value: emailPattern, message: 'Ogiltig epost.' }
+        })}
+        error={errors.username}
+      />
+      <button type="submit">Logga in</button>
+    </Form>
+  );
+};
+
+export default Auth;
