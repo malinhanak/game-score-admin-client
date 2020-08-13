1295421c4d306675f710b23114df62fc3aad3ed6
Malin Hanak <malinehanak@gmail.com>
2020-08-13 12:23:24 +0200

    Comment added

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20,4 +20,5 @@ export const createSlug = (name) => {
     : null;
 };
 
+// eslint-disable-next-line
 export const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
