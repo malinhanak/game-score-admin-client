8fd50b9569bfddb9676e1d92768bea7fea5400e4
Malin Hanak <malinehanak@gmail.com>
2020-08-13 09:04:36 +0200

    ProtectedRoute component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const [cookies] = useCookies(['name']);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (cookies.sid.isOnline) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/auth',
+                state: {
+                  from: props.location
+                }
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
