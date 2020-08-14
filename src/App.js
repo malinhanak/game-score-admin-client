3d0e05bbb28d3324cf94fc1caee5e4a11e4288e0
Malin Hanak <malinehanak@gmail.com>
2020-08-14 15:55:39 +0200

    useEffect removed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { authContext } from './shared/providers/auth-context';
 import Layout from './components/Layout';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
-  const auth = useContext(authContext);
-
-  useEffect(() => {
-    if (auth.isOnline) console.log('Du är eller blev inloggad');
-    console.log('Du är eller blev utloggad');
-  }, [auth]);
-
   return (
     <Layout>
       <Switch>
